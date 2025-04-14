1e657b0aff2f208f0db927cd5de14b023f3b4467
Luan4560 <luan.nascimento.dev@gmail.com>
2025-04-14 18:42:44 -0300

    fix: routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,20 @@
 import { fastify } from "fastify";
 import { routes } from "./http/routes";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { validatorCompiler, serializerCompiler, jsonSchemaTransform} from "fastify-type-provider-zod"
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { fastifyCors } from "@fastify/cors";
 
-
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
-
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
-
-app.register(fastifyCors, {origin: "*"});
+app.register(fastifyCors, { origin: "*" });
 
 app.register(fastifySwagger, {
   openapi: {
@@ -21,14 +22,14 @@ app.register(fastifySwagger, {
       title: "Saloonly API Documentation",
       description: "Saloonly API Documentation",
       version: "1.0.0",
-    }
+    },
   },
 
   transform: jsonSchemaTransform,
-})
+});
 
 app.register(fastifySwaggerUi, {
-  routePrefix: "/docs"
-})
+  routePrefix: "/docs",
+});
 
 app.register(routes);
